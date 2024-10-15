2b7e81f2b2254d405b2cf044157c20959b837277
hasan-abir <contact.hasanabir@gmail.com>
2024-10-15 12:20:11 +0600

    Fixed API URL for client

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,37 +1,40 @@
 import { fileURLToPath, URL } from "node:url";
-
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({
-      autoImport: true,
-    }),
-  ],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "/__tests__/setGlobal.js",
-    deps: {
-      inline: ["vuetify"],
-    },
-  },
-  define: { "process.env": {} },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000/",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      vue(),
+      vuetify({
+        autoImport: true,
+      }),
+    ],
+    test: {
+      environment: "jsdom",
+      globals: true,
+      setupFiles: "/__tests__/setGlobal.js",
+      deps: {
+        inline: ["vuetify"],
       },
     },
-  },
+    define: { __VITE_API_URL__: JSON.stringify(env.VITE_API_URL) },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
